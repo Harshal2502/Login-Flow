01a60b5bc1dbb740531a2a3949214e97e466ab86
HarshalDevmurari <92778686+HarshalDevmurari@users.noreply.github.com>
2022-07-25 00:39:17 +0530

    Make greetings updated dynamically
    It will greet with the first name and last name in real time while adding the names to the input field

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import Login from "./Login";
-
-var isRegistered = false;
+import React, { useState } from "react";
 
 function App() {
+
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function Hover() {
+    setMouseOver(true);
+  }
+  function MouseOut() {
+    setMouseOver(false);
+  }
+
+  const [heading, newHeading] = useState("")
+  const [heading1, newHeading1] = useState("")
+
+  function setHeading(event) {
+    newHeading(event.target.value)
+  }
+  function setHeading1(event) {
+    newHeading1(event.target.value)
+  }
+
   return (
     <div className="container">
-      {isRegistered ? <Login flag={isRegistered} /> : <Login flag={isRegistered} />}
+      <h1>Hello, {heading +" "+ heading1}</h1>
+
+      <form className="form">
+
+        <input onChange={setHeading} type="text" placeholder="Username" />
+        <input onChange={setHeading1} type="text" placeholder="Lastname" />
+        <input type="mail" placeholder="Email" />
+
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          type="submit"
+          onMouseOver={Hover}
+          onMouseOut={MouseOut}>Submit</button>
+
+      </form>
     </div>
   );
 }
