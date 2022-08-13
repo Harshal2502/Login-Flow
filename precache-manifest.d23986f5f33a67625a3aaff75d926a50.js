cf0a9adf3bc8316f89dd75c5027e9f5593572c83
unknown <devmurariharshal2002@gmail.com>
2022-08-13 22:55:02 +0530

    Updates

---
+++ precache-manifest.d23986f5f33a67625a3aaff75d926a50.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4ae24bece9cbe968700ad2bd501945",
+    "url": "/Login-Flow/index.html"
+  },
+  {
+    "revision": "f44635bd619b5b019e01",
+    "url": "/Login-Flow/static/js/2.bc376ad1.chunk.js"
+  },
+  {
+    "revision": "fe4252b1432b4dd97652",
+    "url": "/Login-Flow/static/js/main.738ee311.chunk.js"
+  },
+  {
+    "revision": "ac806db609d676c429c8",
+    "url": "/Login-Flow/static/js/runtime-main.5bf37aec.js"
+  }
+]);
